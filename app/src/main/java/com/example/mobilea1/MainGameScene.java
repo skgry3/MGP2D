1088eb17990dd1532fd3dc99ac61c72b304457c0
skgry3 <joelskgry@gmail.com>
2025-11-17 13:11:46 +0800

    add gamescene bitmap

--- app/src/main/java/com/example/mobilea1/MainGameScene.java
+++ app/src/main/java/com/example/mobilea1/MainGameScene.java
@@ -1,17 +1,40 @@
 package com.example.mobilea1;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.BitmapShader;
 import android.graphics.Canvas;
 
+import com.example.mobilea1.mgp2dCore.GameActivity;
 import com.example.mobilea1.mgp2dCore.GameScene;
 
 public class MainGameScene extends GameScene {
+
+    private Bitmap backgroundBitmap;
+    int screenHeight;
+    int screenWidth;
+
     @Override
-    public void onUpdate(float dt) {
+    public void onCreate()
+    {
+        super.onCreate();
+        screenHeight = GameActivity.instance.getResources().getDisplayMetrics().heightPixels;
+        screenWidth = GameActivity.instance.getResources().getDisplayMetrics().widthPixels;
+
+        Bitmap bmp = BitmapFactory.decodeResource(GameActivity.instance.getResources(), R.drawable.illegelhowchien);
+        backgroundBitmap = Bitmap.createScaledBitmap(bmp, screenWidth, screenHeight, true);
+    }
+
+
+    @Override
+    public void onUpdate(float dt)
+    {
 
     }
 
     @Override
-    public void onRender(Canvas canvas) {
-
+    public void onRender(Canvas canvas)
+    {
+        canvas.drawBitmap(backgroundBitmap, 0,0,null);
     }
 }
